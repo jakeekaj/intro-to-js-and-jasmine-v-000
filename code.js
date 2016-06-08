9daeeef0450c33183ff3f1c57643bda2a37751d2
John Carlo Malto <jakemalto2014@gmail.com>
2016-06-08 03:17:17 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,18 @@
 'use strict';
+
+
+function favIceCream(name){
+  return "I love "+name
+}
+
+function shouting(name){
+  return name.toUpperCase();
+}
+
+function roundDown(num) {
+   return Math.floor(num);
+ }
+
+ function theTruth() {
+   return true
+ }
